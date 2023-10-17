400b593c096e7023784615d3af513971b56f86f2
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-17 20:33:38 +0200

    Add grid content with styling

--- src/app/components/Frame/Frame.jsx
+++ src/app/components/Frame/Frame.jsx
@@ -1,9 +1,34 @@
-import styles from "./Frame.module.css";
+import styles from './Frame.module.css';
 
 export default function Frame() {
   return (
     <>
-      <div className={styles.main}>Hello</div>
+      <div className={styles.gridContainer}>
+        <div className={styles.row1}>
+          <div className={styles.frame75}>Inclusion score</div>
+          <div className={styles.frame25}>Weekly inclusion score</div>
+        </div>
+        <div className={styles.row2}>
+          <div className={styles.leftColumn}>
+            <div className={styles.frame50}>TBD</div>
+            <div className={styles.frame50}>Inclusion of new-hires</div>
+          </div>
+            <div className={styles.frame33}>Differences</div>
+        </div>
+        <div className={styles.row3}>
+          <div className={styles.oneBox}>Trends</div>
+          <div className={styles.rightColumn}>
+            <div className={styles.frame25}>Isolation</div>
+            <div className={styles.frame25}>Segregation</div>
+            <div className={styles.frame25}>Leadership access</div>
+            <div className={styles.frame25}>Onboarding</div>
+            <div className={styles.frame25}>Isolation</div>
+            <div className={styles.frame25}>Segregation</div>
+            <div className={styles.frame25}>Leadership access</div>
+            <div className={styles.frame25}>Onboarding</div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
