27b171d747b29d4a5128a6e9a8e7cb99062d7326
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-17 22:41:29 +0200

    Rename classes and update minor styling changes

--- src/app/components/Frame/Frame.jsx
+++ src/app/components/Frame/Frame.jsx
@@ -4,18 +4,18 @@ export default function Frame() {
   return (
     <>
       <div className={styles.gridContainer}>
-        <div className={styles.row1}>
+        <div className={styles.rowOne}>
           <div className={styles.frame75}>Inclusion score</div>
           <div className={styles.frame25}>Weekly inclusion score</div>
         </div>
-        <div className={styles.row2}>
+        <div className={styles.rowTwo}>
           <div className={styles.leftColumn}>
             <div className={styles.frame50}>TBD</div>
             <div className={styles.frame50}>Inclusion of new-hires</div>
           </div>
-            <div className={styles.frame33}>Differences</div>
+          <div className={styles.frame33}>Differences</div>
         </div>
-        <div className={styles.row3}>
+        <div className={styles.rowThree}>
           <div className={styles.oneBox}>Trends</div>
           <div className={styles.rightColumn}>
             <div className={styles.frame25}>Isolation</div>
