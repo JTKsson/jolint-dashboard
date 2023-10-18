5313d4cd4e04bf47562fdfcaaa2fa5ecbab4e2c5
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-18 11:18:06 +0200

    Add titles + refactor code

--- src/app/components/Frame/Frame.jsx
+++ src/app/components/Frame/Frame.jsx
@@ -1,34 +1,47 @@
 import styles from './Frame.module.css';
 
+const GridItem = ({ className, children }) => (
+  <div className={className}>{children}</div>
+);
+
 export default function Frame() {
   return (
-    <>
-      <div className={styles.gridContainer}>
-        <div className={styles.rowOne}>
-          <div className={styles.frame75}>Inclusion score</div>
-          <div className={styles.frame25}>Weekly inclusion score</div>
+    <div className={styles.gridContainer}>
+      <div className={styles.rowOne}>
+        <GridItem className={styles.frame75}>Inclusion score</GridItem>
+        <GridItem className={styles.frame25}>Weekly inclusion score</GridItem>
+      </div>
+
+      <div className={styles.rowTwo}>
+        <div className={styles.leftColumn}>
+          <GridItem className={styles.frame50}>TBD</GridItem>
+          <GridItem className={styles.frame50}>Inclusion of new-hires</GridItem>
         </div>
-        <div className={styles.rowTwo}>
-          <div className={styles.leftColumn}>
-            <div className={styles.frame50}>TBD</div>
-            <div className={styles.frame50}>Inclusion of new-hires</div>
+        <GridItem className={styles.frame33}>Differences</GridItem>
+      </div>
+
+      <div className={styles.rowThree}>
+        <div className={styles.leftColumnTrends}>
+          <GridItem className={styles.title}>Trends</GridItem>
+          <GridItem className={styles.leftBox}>Trends</GridItem>
+        </div>
+        <div className={styles.rightColumn}>
+          <GridItem className={styles.title}>Risks</GridItem>
+          <div className={styles.rightColumnTop}>
+            <GridItem className={styles.frame25}>Isolation</GridItem>
+            <GridItem className={styles.frame25}>Segregation</GridItem>
+            <GridItem className={styles.frame25}>Leadership access</GridItem>
+            <GridItem className={styles.frame25}>Onboarding</GridItem>
           </div>
-          <div className={styles.frame33}>Differences</div>
-        </div>
-        <div className={styles.rowThree}>
-          <div className={styles.oneBox}>Trends</div>
-          <div className={styles.rightColumn}>
-            <div className={styles.frame25}>Isolation</div>
-            <div className={styles.frame25}>Segregation</div>
-            <div className={styles.frame25}>Leadership access</div>
-            <div className={styles.frame25}>Onboarding</div>
-            <div className={styles.frame25}>Isolation</div>
-            <div className={styles.frame25}>Segregation</div>
-            <div className={styles.frame25}>Leadership access</div>
-            <div className={styles.frame25}>Onboarding</div>
+          <GridItem className={styles.title}>Opportunities</GridItem>
+          <div className={styles.rightColumnBottom}>
+            <GridItem className={styles.frame25}>Isolation</GridItem>
+            <GridItem className={styles.frame25}>Segregation</GridItem>
+            <GridItem className={styles.frame25}>Leadership access</GridItem>
+            <GridItem className={styles.frame25}>Onboarding</GridItem>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
