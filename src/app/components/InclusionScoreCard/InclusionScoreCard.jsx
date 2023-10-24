c7a31d2d034e37797a1beb20d3e9121b1b37ddf1
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-24 19:05:56 +0200

    Add json-file w/ mockData + display the 'Inclusion score' data and round up the decimals

--- src/app/components/InclusionScoreCard/InclusionScoreCard.jsx
+++ src/app/components/InclusionScoreCard/InclusionScoreCard.jsx
@@ -1,7 +1,16 @@
+import inclusionData from '@/app/assets/mockData.json';
+
 export default function InclusionScoreCard() {
+  // Access the "Inclusion score" from the imported JSON data
+  const inclusionScore = inclusionData['Inclusion score'];
+
+  // Round up the decimal to the nearest integer
+  const roundedInclusionScore = Math.ceil(inclusionScore);
+
   return (
     <div>
-      <p>Hej</p>
+      <h1>Inclusion Score</h1>
+      <p>{roundedInclusionScore}</p>
     </div>
-  )
+  );
 }
