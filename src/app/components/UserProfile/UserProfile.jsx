94546b37ded7d487d8d1b3eb4b1f87c16ba704c9
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-24 11:38:51 +0200

    Implement styling to user profile component

--- src/app/components/UserProfile/UserProfile.jsx
+++ src/app/components/UserProfile/UserProfile.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import styles from './userProfile.module.css';
 
 const mockData = [
   {
@@ -11,16 +12,17 @@ export default function UserProfile() {
   const user = mockData[0];
 
   return (
-    <div>
-      <div>
+    <div className={styles.main}>
+      <div className={styles.textLabel}>
         <Image
           src="/images/user-icon.svg"
           alt="User icon"
           width={74}
           height={74}
+          className={styles.image}
         />
       </div>
-      <div>
+      <div className={styles.textLabel}>
         <p>Welcome, {user.name}</p>
         <p>Team {user.role}</p>
       </div>
