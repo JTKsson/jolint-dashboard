0059e0ed23808a21a6805b8a5a635ccb3e928b92
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-24 19:08:10 +0200

    Implement progress bar function w/ simple styling and animation

--- src/app/components/InclusionScoreCard/InclusionScoreCard.jsx
+++ src/app/components/InclusionScoreCard/InclusionScoreCard.jsx
@@ -1,16 +1,28 @@
 import inclusionData from '@/app/assets/mockData.json';
+import styles from './inclusionScoreCard.module.css'
 
-export default function InclusionScoreCard() {
+export default function Frame() {
   // Access the "Inclusion score" from the imported JSON data
   const inclusionScore = inclusionData['Inclusion score'];
 
   // Round up the decimal to the nearest integer
   const roundedInclusionScore = Math.ceil(inclusionScore);
 
+  // Define a CSS style for the progress bar
+  const progressBarStyle = {
+    width: `${roundedInclusionScore}%`,
+    backgroundColor: 'blue',
+    height: '20px',
+    transition: 'width 0.5s ease-in-out',
+  };
+
   return (
     <div>
       <h1>Inclusion Score</h1>
       <p>{roundedInclusionScore}</p>
+      <div className={styles.progressBar}>
+        <div style={progressBarStyle}></div>
+      </div>
     </div>
   );
 }
