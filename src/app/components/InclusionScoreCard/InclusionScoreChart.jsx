d9cfb5b1acf6e9ea4ed21903d3a134b8b3cf04e5
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-25 10:22:10 +0200

    Round up the Inclusion score data and remove the decimals

--- src/app/components/InclusionScoreCard/InclusionScoreChart.jsx
+++ src/app/components/InclusionScoreCard/InclusionScoreChart.jsx
@@ -9,16 +9,22 @@ import {
 } from 'recharts'
 
 export default function ChartComponent({ data }) {
+  // Round up the "Inclusion score" data and remove decimals
+  const roundedData = data.map(item => ({
+    ...item,
+    'Inclusion score': Math.ceil(item['Inclusion score'])
+  }));
+
   return (
     <ResponsiveContainer width="100%" height="70%">
       <AreaChart
         width={500}
         height={400}
-        data={data}
+        data={roundedData} // Use the rounded data
         margin={{ top: 0, right: 2, left: -10, bottom: -7 }}
       >
         <XAxis dataKey="Month" />
-        <YAxis dataKey="Inclusion score" type="number" domain={[0, 100]} /> 
+        <YAxis dataKey="Inclusion score" type="number" domain={[0, 100]} />
         <Tooltip />
         <Area
           type="monotone"
