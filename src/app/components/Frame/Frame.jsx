19aeb9e2ae863d6df32412bd8298d6662aa359b9
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-10-31 14:11:58 +0100

    add risks card component to frame with style

--- src/app/components/Frame/Frame.jsx
+++ src/app/components/Frame/Frame.jsx
@@ -1,4 +1,5 @@
 import InclusionScoreCard from '../InclusionScoreCard/InclusionScoreCard'
+import RisksCard from '../RisksCard'
 import WeeklyInclusionScoreCard from '../WeeklyScoreCard/WeeklyScoreCard'
 import styles from './frame.module.css'
 
@@ -34,10 +35,11 @@ export default function Frame() {
         <div className={styles.rightColumn}>
           <GridItem className={styles.title}>Risks</GridItem>
           <div className={styles.rightColumnTop}>
-            <GridItem className={styles.frame25}>Isolation</GridItem>
+            <RisksCard />
+            {/* <GridItem className={styles.frame25}>Isolation</GridItem>
             <GridItem className={styles.frame25}>Segregation</GridItem>
             <GridItem className={styles.frame25}>Leadership access</GridItem>
-            <GridItem className={styles.frame25}>Onboarding</GridItem>
+            <GridItem className={styles.frame25}>Onboarding</GridItem> */}
           </div>
           <GridItem className={styles.title}>Opportunities</GridItem>
           <div className={styles.rightColumnBottom}>
