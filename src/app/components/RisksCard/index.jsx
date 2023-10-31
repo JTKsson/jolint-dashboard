9195b2a5e7a43d988e56f7dde86667867f06a992
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-10-31 18:19:22 +0100

    remove map index

--- src/app/components/RisksCard/index.jsx
+++ src/app/components/RisksCard/index.jsx
@@ -1,10 +1,10 @@
-import risksData from '../../assets/RisksData.json'
+import risksData from '../../assets/risksData.json'
 import styles from './risksCard.module.css'
 
 const RisksCard = () => {
   return (
     <div className={styles.risksContainer}>
-      {risksData.map((risk, index) => (
+      {risksData.map((risk) => (
         <div className={styles.riskItem}>
           <div className={styles.title}>{risk.title}</div>
           <p>{risk.text}</p>
