628d5d7f9da2b4e801ec002e2c27eb1cf54c9ce5
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-31 12:41:57 +0100

    Implement NewHiresCard components + inital logic and functionality

--- src/app/components/NewHiresCard/NewHiresCard.jsx
+++ src/app/components/NewHiresCard/NewHiresCard.jsx
@@ -1,5 +1,33 @@
-export default function NewHiresCard() { 
+'use client'
+import React, { useState } from 'react';
+import data from '../../assets/newHiresData.json';
+
+export default function NewHiresCard() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  // Extract unique demographic categories
+  const uniqueCategories = [...new Set(data.map(item => item.demographic_category))];
+
+  // Handle category change
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
   return (
-    <h1>Hej</h1>
-  )
+    <div>
+      <label htmlFor='categorySelect' />
+      <select
+        id='categorySelect'
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+      >
+        <option value="">Select a category</option>
+        {uniqueCategories.map((category, index) => (
+          <option key={index} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
