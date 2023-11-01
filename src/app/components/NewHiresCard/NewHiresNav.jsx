1118c1fd08681b2ea07ba156738d2fe35d76eb9d
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-01 19:31:56 +0100

    Add styling + minor code tweaks

--- src/app/components/NewHiresCard/NewHiresNav.jsx
+++ src/app/components/NewHiresCard/NewHiresNav.jsx
@@ -1,3 +1,5 @@
+import styles from './newHiresCard.module.css'
+
 export default function NewHiresNav(props) {
   const {
     uniqueCategories,
@@ -9,31 +11,36 @@ export default function NewHiresNav(props) {
   } = props
 
   return (
-    <nav>
-      <label htmlFor="categorySelect" />
-      <select
-        id="categorySelect"
-        value={selectedCategory}
-        onChange={handleCategoryChange}
-      >
-        {uniqueCategories.map((category, index) => (
-          <option key={index} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      <label htmlFor="metricSelect" />
-      <select
-        id="metricSelect"
-        value={selectedMetric}
-        onChange={handleMetricChange}
-      >
-        {uniqueMetrics.map((metric, index) => (
-          <option key={index} value={metric}>
-            {metric}
-          </option>
-        ))}
-      </select>
+    <nav className={styles.nav}>
+      <div className={styles.title}>
+        <h1>Inclusion of new-hires</h1>
+      </div>
+      <div className={styles.dropdown}>
+        <label htmlFor="categorySelect" />
+        <select
+          id="categorySelect"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          {uniqueCategories.map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="metricSelect" />
+        <select
+          id="metricSelect"
+          value={selectedMetric}
+          onChange={handleMetricChange}
+        >
+          {uniqueMetrics.map((metric, index) => (
+            <option key={index} value={metric}>
+              {metric}
+            </option>
+          ))}
+        </select>
+      </div>
     </nav>
   )
 }
