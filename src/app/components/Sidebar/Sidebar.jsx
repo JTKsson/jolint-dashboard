49fab097ea436b5f32f298dbf6bec6456e2429d3
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-01 22:39:15 +0100

    Add Jolint logo and signout item w/ styling

--- src/app/components/Sidebar/Sidebar.jsx
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -5,26 +5,47 @@ import UserProfile from '../UserProfile/UserProfile'
 export default function Sidebar() {
   return (
     <div className={styles.main}>
-      <div className={styles.userContainer}>
-        <UserProfile />
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Image
+            src="/images/JOLINT-logo.svg"
+            alt="Jolint logo"
+            width={100}
+            height={100}
+            />
+        </div>
+        <div className={styles.userContainer}>
+          <UserProfile />
+        </div>
+        <div className={styles.buttonItem}>
+          <Image
+            src="/images/dashboard-icon.svg"
+            alt="Dashboard icon"
+            width={20}
+            height={20}
+            />
+          <p>Dashboard</p>
+        </div>
+        <div className={styles.buttonItem}>
+          <Image
+            src="/images/settings-icon.svg"
+            alt="Settings icon"
+            width={20}
+            height={20}
+            />
+          <p>Settings</p>
+        </div>
       </div>
-      <div className={styles.textLabel}>
-        <Image
-          src="/images/dashboard-icon.svg"
-          alt="Dashboard icon"
-          width={20}
-          height={20}
-        />
-        <p>Dashboard</p>
-      </div>
-      <div className={styles.textLabel}>
-        <Image
-          src="/images/settings-icon.svg"
-          alt="Settings icon"
-          width={20}
-          height={20}
-        />
-        <p>Settings</p>
+      <div>
+        <div className={styles.buttonItem}>
+          <Image
+            src="/images/signout-icon.svg"
+            alt="Signout icon"
+            width={20}
+            height={20}
+            />
+          <p className={styles.signOutText}>Sign out</p>
+        </div>
       </div>
     </div>
   )
