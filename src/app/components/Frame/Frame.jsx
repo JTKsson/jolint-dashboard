852193b9bac973db4e29d9f17b65b4d3676ae8ae
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-02 13:50:52 +0100

    Bugfix: resolve merge

--- src/app/components/Frame/Frame.jsx
+++ src/app/components/Frame/Frame.jsx
@@ -47,27 +47,23 @@ export default function Frame() {
           <GridItem className={styles.frame33}>Differences</GridItem>
         </div>
 
-        <div className={styles.rowThree}>
-          <div className={styles.leftColumnTrends}>
-            <GridItem className={styles.title}>Trends</GridItem>
-            <GridItem className={styles.leftBox}>
-            
-              <TrendsCard />
-          
-            </GridItem>
+      <div className={styles.rowThree}>
+        <div className={styles.leftColumnTrends}>
+          <GridItem className={styles.title}>Trends</GridItem>
+          <GridItem className={styles.leftBox}><TrendsCard /></GridItem>
+        </div>
+        <div className={styles.rightColumn}>
+          <GridItem className={styles.title}>Risks</GridItem>
+          <div className={styles.frame50}>
+            <RisksCard />
           </div>
-          <div className={styles.rightColumn}>
-            <GridItem className={styles.title}>Risks</GridItem>
-            <div className={styles.rightColumnTop}>
-              <RisksCard />
-            </div>
-            <GridItem className={styles.title}>Opportunities</GridItem>
-            <div className={styles.rightColumnBottom}>
-              <OpptsCard />
-            </div>
+          <GridItem className={styles.title}>Opportunities</GridItem>
+          <div className={styles.frame50}>
+            <OpptsCard />
           </div>
         </div>
       </div>
-    </div>
+      </div>
+     </div> 
   )
 }
