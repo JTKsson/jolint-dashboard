dec61b9ed6591eff04db119855882bc438cb782f
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-11-08 12:12:39 +0100

    add differences nav component

---
+++ src/app/components/DifferencesCard/DifferencesNav.jsx
@@ -0,0 +1,31 @@
+import styles from "./differencesNav.module.css"
+
+export default function DifferencesNav(props) {
+    const {
+      uniqueCategories,
+      selectedCategory,
+      handleCategoryChange
+    } = props
+  
+    return (
+      <nav className={styles.nav}>
+        <div className={styles.title}>
+          <h1>DifferencesNav</h1>
+        </div>
+        <div className={styles.dropdown}>
+          <label htmlFor="categorySelect" />
+          <select
+            id="categorySelect"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+          >
+            {uniqueCategories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      </nav>
+    )
+  }
