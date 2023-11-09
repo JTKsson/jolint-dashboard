33f3f6a90e0a346c34233f18a11ac3af7465dc1a
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-11-09 16:30:18 +0100

    add (hard coded) data plots and styling to differences chart

--- src/app/components/DifferencesCard/DifferencesChart.jsx
+++ src/app/components/DifferencesCard/DifferencesChart.jsx
@@ -2,40 +2,37 @@
 import { useState, useEffect } from 'react'
 import {
   BarChart,
+  CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   Legend,
   ResponsiveContainer,
-  Bar
+  Bar,
+  Label,
+  LabelList,
 } from 'recharts'
-
 import DifferencesNav from './DifferencesNav'
-import styles from './differencesCard.module.css'
 
 export default function DifferencesCardChart({ data }) {
+  const [selectedCategory, setSelectedCategory] = useState(
+    data[0]?.demographic_category,
+  ) // Initialize with the first category
 
-    // console.log(data);
+  const uniqueCategories = [
+    ...new Set(data.map((item) => item.demographic_category)), // Extract unique demographic categories
+  ]
 
-   const [selectedCategory, setSelectedCategory] = useState(
-        data[0]?.demographic_category,
-    ) // Initialize with the first category
+  //   console.log(uniqueCategories, "uniqueCategories");
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value) // Handle category change
+  }
 
-    const uniqueCategories = [
-        ...new Set(data.map((item) => item.demographic_category)), // Extract unique demographic categories
-    ]
+  useEffect(() => {
+    setSelectedCategory(data[0]?.demographic_category) // This effect sets the selectedCategory to the first category when the component mounts
+  }, [])
 
-    //   console.log(uniqueCategories, "uniqueCategories");
-    const handleCategoryChange = (e) => {
-        setSelectedCategory(e.target.value) // Handle category change
-    }
-
-    useEffect(() => {
-        setSelectedCategory(data[0]?.demographic_category) // This effect sets the selectedCategory to the first category when the component mounts
-    }, [])
-
-    console.log("selectedCategory", selectedCategory)
-
+  console.log('selectedCategory', selectedCategory)
 
   const roundedData = data.map((item) => ({
     ...item,
@@ -62,28 +59,89 @@ export default function DifferencesCardChart({ data }) {
       results.push(result)
     })
 
-    // console.log("RESULTS WorkCopy", results)
-
     selectedData = results.filter(
-      (item) =>
-          item.demographic_category === selectedCategory
-  )
-
-//   console.log("selectedData", selectedData)
+      (item) => item.demographic_category === selectedCategory,
+    )
   })
 
-  const plotData = selectedData.map(obj => {
-    const { demographic_category, ...plotObj} = obj;
-    return plotObj;
+  const plotData = selectedData.map((obj) => {
+    const { demographic_category, ...plotObj } = obj
+    return plotObj
   })
 
-  console.log(plotData);
+  console.log(plotData)
+
   return (
-   <>
-        <DifferencesNav 
+    <>
+      <DifferencesNav
         uniqueCategories={uniqueCategories}
         selectedCategory={selectedCategory}
-        handleCategoryChange={handleCategoryChange} />
-   </>
+        handleCategoryChange={handleCategoryChange}
+      />
+      <ResponsiveContainer width="100%" height={250}>
+        <BarChart
+          data={plotData}
+          margin={{ top: 0, right: 10, left: -38, bottom: -7 }}
+        >
+          <XAxis
+            dataKey="metric"
+            tick={{ fontSize: 10 }}
+            stroke="var(----text-color-dark)"
+            text={5}
+          />
+          <YAxis
+            type="number"
+            stroke="var(----text-color-dark)"
+            tick={{ fontSize: 12 }}
+          />
+          <Legend
+            layout="horizontal"
+            verticalAlign="top"
+            align="right"
+            iconType="circle"
+          />
+          <Tooltip />
+          {selectedCategory === 'Gender' && (
+            <>
+              <Bar dataKey="Male" fill="var(--c-mellow-blue)" />
+              <Bar dataKey="Female" fill="var(--c-light-red)" />
+              <Bar dataKey="Non-Binary" fill="var(--c-orange)" />
+              <LabelList dataKey="metric" position="top" />
+            </>
+          )}
+          {selectedCategory === 'Age Group' && (
+            <>
+              <Bar dataKey="18-25" fill="var(--c-mellow-blue)" />
+              <Bar dataKey="26-35" fill="var(--c-light-red)" />
+              <Bar dataKey="36-45" fill="var(--c-orange)" />
+              <Bar dataKey="46-55" fill="var(--c-bright-blue)" />
+              <Bar dataKey="56+" fill="var(--c-dark-gray)" />
+              <LabelList dataKey="metric" position="top" />
+            </>
+          )}
+          {selectedCategory === 'Background' && (
+            <>
+              <Bar dataKey="Immigrant" fill="var(--c-mellow-blue)" />
+              <Bar dataKey="native" fill="var(--c-light-red)" />
+              <LabelList dataKey="metric" position="top" />
+            </>
+          )}
+          {selectedCategory === 'Ethnicity' && (
+            <>
+              <Bar dataKey="Majority" fill="var(--c-mellow-blue)" />
+              <Bar dataKey="Minority" fill="var(--c-light-red)" />
+              <LabelList dataKey="metric" position="top" />
+            </>
+          )}
+          {selectedCategory === 'Sexual Orientation' && (
+            <>
+              <Bar dataKey="hetero" fill="var(--c-mellow-blue)" />
+              <Bar dataKey="LBGTQ+" fill="var(--c-light-red)" />
+              <LabelList dataKey="metric" position="top" />
+            </>
+          )}
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   )
 }
