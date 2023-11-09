4cdaacd440c39e9fe6ff934551ef179d13362387
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-11-09 16:25:32 +0100

    add main styling to differences card

--- src/app/components/DifferencesCard/DifferencesCard.jsx
+++ src/app/components/DifferencesCard/DifferencesCard.jsx
@@ -2,11 +2,12 @@
 import differencesData from '../../assets/differencesData.json'
 import DifferencesChart from './DifferencesChart'
 import DifferencesNav from './DifferencesNav'
+import styles from './differencesCard.module.css'
 
 export default function DifferencesCard() {
   return (
-    <div>
-        <DifferencesChart data={differencesData} />
+    <div className={styles.main}>
+      <DifferencesChart data={differencesData} />
     </div>
   )
 }
