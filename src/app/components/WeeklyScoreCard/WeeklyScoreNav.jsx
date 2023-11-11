a771023a1b6d25a310079578ff9424fb23daaf2b
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-11 20:36:17 +0100

    Remove overflow: hidden on card

--- src/app/components/WeeklyScoreCard/WeeklyScoreNav.jsx
+++ src/app/components/WeeklyScoreCard/WeeklyScoreNav.jsx
@@ -3,32 +3,16 @@ import styles from './weeklyScoreCard.module.css'
 import Image from 'next/image'
 
 export default function InclusionScoreCard({ thisWeek }) {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen)
-  }
 
   return (
     <div className={styles.nav}>
       <div className={styles.title}>
         <h2>Weekly inclusion score</h2>
       </div>
-      <div className={styles.dropdown}>
-        <button onClick={toggleDropdown} className={styles.button}>
-          <div>This week</div>
-          <div>
-            <Image
-              src="/images/arrow-down-s-line.svg"
-              alt="Arrow down icon"
-              width={8}
-              height={8}
-            />
-          </div>
-        </button>
-        {isDropdownOpen && (
-          <div className={styles.dropdownContent}>{/* Add content here */}</div>
-        )}
+      <div>
+        <select>
+          <option>This week</option>
+        </select>
       </div>
     </div>
   )
