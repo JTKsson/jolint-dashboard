a899321eb506e764c8d421b30216cdaa11eb2252
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-11-11 18:38:03 +0100

    add styling to nav items div

--- src/app/components/DifferencesCard/DifferencesNav.jsx
+++ src/app/components/DifferencesCard/DifferencesNav.jsx
@@ -9,20 +9,22 @@ export default function DifferencesNav(props) {
       <div className={styles.title}>
         <h1>Differences</h1>
       </div>
-      <CustomLegend selectedCategory={selectedCategory} />
-      <div className={styles.dropdown}>
-        <label htmlFor="categorySelect" />
-        <select
-          id="categorySelect"
-          value={selectedCategory}
-          onChange={handleCategoryChange}
-        >
-          {uniqueCategories.map((category, index) => (
-            <option key={index} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
+      <div className={styles.navItems}>
+        <CustomLegend selectedCategory={selectedCategory} />
+        <div className={styles.dropdown}>
+          <label htmlFor="categorySelect" />
+          <select
+            id="categorySelect"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+          >
+            {uniqueCategories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </nav>
   )
