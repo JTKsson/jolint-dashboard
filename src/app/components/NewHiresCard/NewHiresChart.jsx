10260dca6bf3ce771d08159408ae1807a753f00c
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-11-11 15:45:29 +0100

    add new sort of newHires data array, plot and legend

--- src/app/components/NewHiresCard/NewHiresChart.jsx
+++ src/app/components/NewHiresCard/NewHiresChart.jsx
@@ -8,6 +8,7 @@ import {
   YAxis,
   Tooltip,
   ResponsiveContainer,
+  Legend,
 } from 'recharts'
 import NewHiresNav from './NewHiresNav'
 import styles from './newHiresCard.module.css'
@@ -15,36 +16,55 @@ import styles from './newHiresCard.module.css'
 export default function NewHiresChart() {
   const [selectedCategory, setSelectedCategory] = useState(
     data[0]?.demographic_category,
-  ) // Initialize with the first category
-  const [selectedMetric, setSelectedMetric] = useState('Team Inclusion') // Default metric
+  )
 
   const uniqueCategories = [
-    ...new Set(data.map((item) => item.demographic_category)), // Extract unique demographic categories
+    ...new Set(data.map((item) => item.demographic_category)),
   ]
 
-  const uniqueMetrics = [...new Set(data.map((item) => item.metric))] // Extract unique metrics
-
   const handleCategoryChange = (e) => {
-    setSelectedCategory(e.target.value) // Handle category change
-  }
-  const handleMetricChange = (e) => {
-    setSelectedMetric(e.target.value) // Handle metric change
+    setSelectedCategory(e.target.value)
   }
 
   useEffect(() => {
-    setSelectedCategory(data[0]?.demographic_category) // This effect sets the selectedCategory to the first category when the component mounts
+    setSelectedCategory(data[0]?.demographic_category)
   }, [])
 
-  // Filter the data based on the selected category and metric
-  const filteredData = data.filter(
-    (item) =>
-      item.demographic_category === selectedCategory &&
-      item.metric === selectedMetric,
-  )
+  const roundedData = data.map((item) => ({
+    ...item,
+    inclusion_value: Math.round(item['inclusion_value'] * 100) / 100,
+  }))
 
-  filteredData.forEach((item) => {
-    item.variation = Math.floor(item.variation) // Round down the "variation" value for each data point
+  const results = []
+  let selectedData = []
+
+  const uniqueTimes = [...new Set(data.map((item) => item.time))]
+
+  uniqueCategories.forEach((category) => {
+    uniqueTimes.forEach((time) => {
+      const filteredData = roundedData.filter(
+        (item) => item.demographic_category === category && item.time === time,
+      )
+
+      const result = { time, demographic_category: category }
+      filteredData.forEach((item) => {
+        result[item.demographic_value] = item.inclusion_value
+      })
+
+      results.push(result)
+    })
+
+    selectedData = results.filter(
+      (item) => item.demographic_category === selectedCategory,
+    )
   })
+
+  const plotData = selectedData.map((obj) => {
+    const { demographic_category, ...plotObj } = obj
+    return plotObj
+  })
+
+  console.log(plotData)
 
   return (
     <>
@@ -52,17 +72,39 @@ export default function NewHiresChart() {
         uniqueCategories={uniqueCategories}
         selectedCategory={selectedCategory}
         handleCategoryChange={handleCategoryChange}
-        uniqueMetrics={uniqueMetrics}
-        selectedMetric={selectedMetric}
-        handleMetricChange={handleMetricChange}
       />
+
       <ResponsiveContainer width="100%" height={250}>
         <AreaChart
-          data={filteredData}
+          data={plotData}
           margin={{ top: 0, right: 10, left: -38, bottom: -7 }}
         >
           <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="mellow-blue" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--c-mellow-blue)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--c-mellow-blue)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="light-red" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--c-light-red)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--c-light-red)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="bright-blue" x1="0" y1="0" x2="0" y2="1">
               <stop
                 offset="5%"
                 stopColor="var(--c-bright-blue)"
@@ -74,26 +116,194 @@ export default function NewHiresChart() {
                 stopOpacity={0}
               />
             </linearGradient>
+            <linearGradient id="bleachedalmond" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--c-bleachedalmond)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--c-bleachedalmond)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="bright-blue" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--c-bright-blue)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--c-bright-blue)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+            <linearGradient id="dark-gray" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor="var(--c-dark-gray)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor="var(--c-dark-gray)"
+                stopOpacity={0}
+              />
+            </linearGradient>
           </defs>
           <XAxis
             tick={{ fontSize: 12 }}
             stroke="var(----text-color-dark)"
-            dataKey="demographic_value"
+            dataKey="time"
           />
-          <YAxis
-            stroke="var(----text-color-dark)"
-            tick={{ fontSize: 12 }}
-            dataKey="variation"
+          <YAxis stroke="var(----text-color-dark)" tick={{ fontSize: 12 }} />
+          <Legend
+            layout="horizontal"
+            verticalAlign="top"
+            align="right"
+            iconType="circle"
+            iconSize={10}
           />
           <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="variation"
-            name={selectedMetric}
-            stroke="var(--c-bright-blue)"
-            fill="url(#colorUv)"
-            strokeWidth={2.3}
-          />
+          {selectedCategory === 'Gender' && (
+            <>
+              <Area
+                type="monotone"
+                dataKey="Male"
+                name={'Male'}
+                stroke="var(--c-mellow-blue)"
+                fill="url(#mellow-blue)"
+                strokeWidth={2.3}
+              />
+
+              <Area
+                type="monotone"
+                dataKey="Female"
+                name={'Female'}
+                stroke="var(--c-light-red)"
+                fill="url(#light-red)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="Non-Binary"
+                name={'Non-Binary'}
+                stroke="var(--c-orange)"
+                fill="url(#bleachedalmond)"
+                strokeWidth={2.3}
+              />
+            </>
+          )}
+
+          {selectedCategory === 'Age Group' && (
+            <>
+              <Area
+                type="monotone"
+                dataKey="18-25"
+                name={'18-25'}
+                stroke="var(--c-mellow-blue)"
+                fill="url(#mellow-blue)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="26-35"
+                name={'26-35'}
+                stroke="var(--c-light-red)"
+                fill="url(#light-red)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="36-45"
+                name={'36-45'}
+                stroke="var(--c-orange)"
+                fill="url(#orange)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="46-55"
+                name={'46-55'}
+                stroke="var(--c-bright-blue)"
+                fill="url(#bright-blue)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="56+"
+                name={'56+'}
+                stroke="var(--c-dark-gray)"
+                fill="url(#dark-gray)"
+                strokeWidth={2.3}
+              />
+            </>
+          )}
+
+          {selectedCategory === 'Background' && (
+            <>
+              <Area
+                type="monotone"
+                dataKey="Immigrant"
+                name={'Immigrant'}
+                stroke="var(--c-mellow-blue)"
+                fill="url(#mellow-blue)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="native"
+                name={'Native'}
+                stroke="var(--c-light-red)"
+                fill="url(#light-red)"
+                strokeWidth={2.3}
+              />
+            </>
+          )}
+
+          {selectedCategory === 'Ethnicity' && (
+            <>
+              <Area
+                type="monotone"
+                dataKey="Majority"
+                name={'Majority'}
+                stroke="var(--c-mellow-blue)"
+                fill="url(#mellow-blue)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="Minority"
+                name={'Minority'}
+                stroke="var(--c-light-red)"
+                fill="url(#light-red)"
+                strokeWidth={2.3}
+              />
+            </>
+          )}
+
+          {selectedCategory === 'Sexual Orientation' && (
+            <>
+              <Area
+                type="monotone"
+                dataKey="hetero"
+                name={'Hetero'}
+                stroke="var(--c-mellow-blue)"
+                fill="url(#mellow-blue)"
+                strokeWidth={2.3}
+              />
+              <Area
+                type="monotone"
+                dataKey="LBGTQ+"
+                name={'LGBTQ+'}
+                stroke="var(--c-light-red)"
+                fill="url(#light-red)"
+                strokeWidth={2.3}
+              />
+            </>
+          )}
         </AreaChart>
       </ResponsiveContainer>
     </>
